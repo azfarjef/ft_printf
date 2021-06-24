48308f44471d674fceb34d3ed8300dc18c49df50
azfarjef <muhammadazfarjef@gmail.com>
2021-06-24 09:46:28 +0800

    2nd commit

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mahmad-j <mahmad-j@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 23:05:17 by mahmad-j          #+#    #+#             */
-/*   Updated: 2021/06/24 01:48:50 by mahmad-j         ###   ########.fr       */
+/*   Updated: 2021/06/24 09:38:55 by mahmad-j         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,21 @@ typedef struct	s_set
 	int			point;
 }				t_set;
 
-void	ft_settings(t_set *set)
+enum			e_func
+{
+	e_char,
+	e_string,
+	e_pointer,
+	e_int,
+	e_u_int,
+	e_u_hex_int,
+	e_percent,
+	e_len
+};
+
+typedef void	t_func(t_set *set);
+
+void	ft_settings(t_set *set);
 
 
 #endif
--- source/ft_settings.c
+++ source/ft_settings.c
@@ -6,7 +6,7 @@
 /*   By: mahmad-j <mahmad-j@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/24 01:47:54 by mahmad-j          #+#    #+#             */
-/*   Updated: 2021/06/24 01:49:30 by mahmad-j         ###   ########.fr       */
+/*   Updated: 2021/06/24 09:41:26 by mahmad-j         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,16 @@
 
 void	ft_settings(t_set *set)
 {
-	int			num;
-	static	t_formatter
+	int				num;
+	static	t_func	*func_arr[e_len] = {
+	[e_char] = ft_solve_char,
+	[e_string] = ft_solve_string,
+	[e_pointer] = ft_solve_pointer,
+	[e_int] = ft_solve_int,
+	[e_u_int] = ft_solve_uint,
+	[e_u_hex_int] = ft_solve_hex,
+	[e_percent] = ft_solve_percent};
+
+	set->format++;
+	
 }
